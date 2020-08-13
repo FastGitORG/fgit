4839581824fac6e13944f043da041926ab8f7ca5
mivik <mivik@qq.com>
2020-08-13 20:55:08 +0800

    Fix several bugs mainly in fgit.sh; Correct many grammar mistakes; Update README.md.

---
+++ scripts/fgit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# author: xkeyC
+# url: github.com/xkeyC
+
+# Check arguments
+if [ "$1" == "" ];then 
+    echo Error: expected a GitHub repository URL after fgit.
+    exit 1
+fi
+
+# Check URL
+if [[ "$1" != *"github.com"* ]]
+then
+echo "Error: $1 is not a GitHub URL!"
+exit 1
+fi
+
+first=${1/"github.com"/"hub.fastgit.org"}
+shift
+
+# Replace URL and call git
+git clone $first $@
